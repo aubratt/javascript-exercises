7fece324bfffc88b31b4c1ccc727eaa257658d20
Austin <ausbratton@gmail.com>
2025-08-18 10:03:37 -0500

    Complete exercise

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+const repeatString = (string, num) => {
+    if (num < 1) {
+        return "Please enter a positive number";
+    }
 
+    let repeated = "";
+    for (let i = 0; i < num; i++) {
+        repeated += string;
+    }
+    return repeated; 
 };
+
+console.log(repeatString("hey", -1));
 
 // Do not edit below this line
 module.exports = repeatString;
