3ee76e57a6dd50f6b126dee41ada0014c4b3551d
Austin <ausbratton@gmail.com>
2025-08-18 10:30:54 -0500

    Complete exercise

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,17 @@
-const reverseString = function() {
+// const reverseString = (string) => {
+//     let chars = [];
+//     for (let i = string.length - 1; i >= 0; i--) {
+//         chars += string[i];
+//     }
+//     return chars;
+// };
 
+// console.log(reverseString("Hello, World!"));
+
+// My solution above works, but the solution below is more concise.
+
+const reverseString = (string) => {
+    return string.split("").reverse().join("");
 };
 
 // Do not edit below this line
