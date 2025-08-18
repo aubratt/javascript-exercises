e956a0dabe4748693dd3d6a338e6c821efb7f921
Austin <ausbratton@gmail.com>
2025-08-18 11:17:29 -0500

    Complete exercise

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,8 @@
-const leapYears = function() {
-
+const leapYears = (year) => {
+    return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0; 
 };
+
+console.log(leapYears(2004));
 
 // Do not edit below this line
 module.exports = leapYears;
