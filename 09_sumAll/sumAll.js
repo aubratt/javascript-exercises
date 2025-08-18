8b23a7d8dd1da4d5b5d264c0981d635e27f95c48
Austin <ausbratton@gmail.com>
2025-08-18 10:56:38 -0500

    Complete exercise

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,14 @@
-const sumAll = function() {
+const sumAll = (firstInt, secondInt) => {
+    let sum = 0;
 
+    for (let i = firstInt; i <= secondInt; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
+
+console.log(sumAll(1, 4));
 
 // Do not edit below this line
 module.exports = sumAll;
